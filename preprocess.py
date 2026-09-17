833a2a66f562c25bd76c95405891f1c4391c9c29
anniemx <163184360+anniemx@users.noreply.github.com>
2026-09-17 18:38:31 +0300

    Add Preprocess file

---
+++ preprocess.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import re
+
+
+
+def extraxt_headlines(corpus):
+    """
+    Args: Corpus from the vrt-fil with headings, image captions, textbody etc.
+
+    Returns: List, extracted headlines by the tag <sentence>, where type="heading".
+
+    """
+
+    headlines = []
+    for hdl in re.findall(r"<sentence[^>]*type=\"heading\"[^>]*>(.*?)</sentence>", str(corpus.read()), re.DOTALL):
+        headlines.append(hdl)
+
+    print(headlines[2]) #for testing
+    print(len(headlines)) #for testing
+
+    return headlines
+
+
+if __name__ == "__main__":
+    corpus = open("01.vrt", "r", encoding="utf8")   #for testing, can be automated later
+    extraxt_headlines(corpus)
